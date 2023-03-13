5abdafebc3c477286de22aa5bbf02bc8c75828e8
Maria17122003 <123393865+Maria17122003@users.noreply.github.com>
2023-03-13 21:06:20 +1000

    Update main.py

--- main.py
+++ main.py
@@ -98,8 +98,10 @@ class Video:
         self.video_id = video_id
         api_key: str = os.getenv('api_key')
         youtube = build('youtube', 'v3', developerKey=api_key)
-        self.video = youtube.videos().list(id=video_id, part='snippet').execute()
+        self.video = youtube.videos().list(part='snippet,statistics', id=video_id).execute()
         self.video_name = self.video['items'][0]['snippet']['title']
+        self.view_count = self.video['items'][0]['statistics']['viewCount']
+        self.like_count = self.video['items'][0]['statistics']['likeCount']
 
     def __str__(self):
         return f'{self.video_name}'
@@ -116,8 +118,11 @@ class PLVideo(Video):
         api_key: str = os.getenv('api_key')
         youtube = build('youtube', 'v3', developerKey=api_key)
         self.playlist_id = playlist_id
-        self.playlist = youtube.playlists().list(id=playlist_id, part='contentDetails,snippet', maxResults=50).execute()
+        self.playlist = youtube.playlists().list(id=playlist_id, part='snippet, contentDetails, status').execute()
         self.playlist_name = self.playlist['items'][0]['snippet']['title']
+        self.video_name = self.video['items'][0]['snippet']['title']
+        self.view_count = self.video['items'][0]['statistics']['viewCount']
+        self.like_count = self.video['items'][0]['statistics']['likeCount']
 
     def __str__(self):
         return f'{self.video_name} ({self.playlist_name})'
